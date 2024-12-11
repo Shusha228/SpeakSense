9f726b69c3a3f0e91c690aad26616eee0ab5c981
Egor <74248162+KRUS-LING@users.noreply.github.com>
2024-12-11 16:56:15 +0500

    Add code for the prototype

---
+++ Prototype/main.py
@@ -0,0 +1,24 @@
+from kivy.core.window import Window
+from kivy.lang import Builder
+
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+
+Window.size = (393, 852)
+
+
+class WelcomeScreen(MDScreen):
+    pass
+
+
+class MainScreen(MDScreen):
+    pass
+
+
+class Example(MDApp):
+
+    def build(self):
+        return Builder.load_file("main.kv")
+
+
+Example().run()
